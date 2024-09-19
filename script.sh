9e2397fa677e64af975c1643a99b37393cceacc3
Ricky <68834779+rickyshark@users.noreply.github.com>
2024-09-19 13:40:59 -0500

    Configurando no de js en mi workflow

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-sudo apt-get update
+sudo apt-get update #Actualizar repositorio
 
-sudo apt-get install curl
+sudo apt-get install curl -y #Instalando el paquete curl. -y si quiero instalar el paquete, ya que no puedo tener ninguna interacción con los servidores de actions.
